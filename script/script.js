1b82c7607d1b3796c348825add2e545950700e14
luqman hanif <m.lujmanhanif@gmail.com>
2019-06-18 15:32:49 +0700

    js tmbahan

--- script/script.js
+++ script/script.js
@@ -13,6 +13,103 @@ function goTesti(){
     document.location = "testimoni.html";
 }
 
+
+function testiValidation(){
+
+    if(!checkNameFill()){
+        alert("you must fill this name field");
+        event.preventDefault();
+
+    }else if(!checkNameIsNumber()){
+        alert("you cant use number in this field");
+        event.preventDefault();
+
+    }else if(!checkRate()){
+        alert("you must fill this name field");
+        event.preventDefault();
+    }else if(!checkPhoneFill()){
+        alert("you must fill this name field");
+        event.preventDefault();    
+    }else if(checkCommentValidation()){
+        alert("you must fill this field more than 10 characters");
+
+    }
+
+
+}
+
+function checkNameFill(){
+    var checkName = document.getElementById('name').value;
+    if(checkName == ' '){
+        return false;
+
+    }else{
+        return true;
+
+    }
+
+
+}
+
+function checkNameIsNumber(){
+    var name = document.getElementById('name').value;
+    var flagCheckNumber = false;
+
+    for(let i = 0; i < name.length; i++){
+        if(!isNaN(name[i])){
+            flagCheckNumber = true;
+        }
+
+    }
+
+    if(flagCheckNumber == true){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+function checkRate(){
+    var flag = document.getElementById('rate'.value);
+
+    if(flag == ' '){
+        return false;
+
+    }else{
+        return true;
+    }
+
+
+
+}
+
+function checkPhoneFill(){
+
+    var checkPhone = document.getElementById('phone').value;
+    if(checkPhone == ' '){
+        return false;
+
+    }else{
+        return true;
+
+    }
+
+}
+
+function checkCommentValidation(){
+    var checkComment = document.getElementById('comment').value;
+
+    if(checkComment.length < 10){
+        return false;
+    }else{
+        return true;
+    }
+
+
+
+}
+
 $(document).ready(function(){
 
     var totalPic = $('.slide').length;
